d74828fa33c530786ed7afe1479577e6ef88038e
jbxamora <jbxamora@icloud.com>
2023-05-04 13:36:53 -0700

    created user schema

--- server/index.js
+++ server/index.js
@@ -8,13 +8,16 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 // CONFIG
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+
 const app = express();
 
+// MIDDLEWARE
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -35,6 +38,10 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload,single("picture"), register);
+
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 3001;
