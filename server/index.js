a78b9c2a9654e39ce756191fc43b0eba1ad9d9f9
jbxamora <jbxamora@icloud.com>
2023-05-04 14:01:36 -0700

    authentication set up

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 // CONFIG
@@ -42,6 +44,9 @@ const upload = multer({ storage });
 // ROUTES WITH FILES
 app.post("/auth/register", upload,single("picture"), register);
 
+// ROUTES
+app.use("/auth", authRoutes)
+
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 3001;
@@ -56,3 +61,4 @@ mongoose
     });
   })
   .catch((error) => console.log(`${error} did not connect`));
+
