5e6e6f9922d8d9fc3be1e9fa3a77fc72555bc388
jbxamora <jbxamora@icloud.com>
2023-05-06 15:12:43 -0700

    updated client, created global state logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BroswerRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
   return (
     <div className="app">
-      
+      <BroswerRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </Routes>
+      </BroswerRouter>
     </div>
   );
 }
