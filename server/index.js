54826e53b21d1ea1a0595d8f023561bedc57c253
jbxamora <jbxamora@icloud.com>
2023-05-06 14:03:31 -0700

    created post routes

--- server/index.js
+++ server/index.js
@@ -11,8 +11,11 @@ import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
 
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/post.js";
 
 // CONFIG
 const __filename = fileURLToPath(import.meta.url);
@@ -45,10 +48,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload,single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 3001;
